4383d7d0d57c5af850fb23a621690adc3f9d0aca
geqi <jiangsugeqi@outlook.com>
2019-01-04 20:48:53 +0800

    add 205

--- src/main/java/com/geqi/leetcode/Isomorphic205.java
+++ src/main/java/com/geqi/leetcode/Isomorphic205.java
@@ -7,10 +7,8 @@ import java.util.Objects;
 public class Isomorphic205 {
 
     public boolean isIsomorphic(String s, String t) {
-
         Map<Character,Integer> m1=new HashMap<>(256);
         Map<Character,Integer> m2=new HashMap<>(256);
-
         for(int i=0;i<s.length();i++){
             if(!Objects.equals(m1.get(s.charAt(i)),m2.get(t.charAt(i)))){
                 return false;
